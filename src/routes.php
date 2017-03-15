158017ccb4845c8c171174210a1da6bf5127c838
Stefan Skliarov <serg.osipchuk@rapidapi.com>
2017-03-15 15:31:10 +0200

    init commit

---
+++ src/routes.php
@@ -0,0 +1,10 @@
+       <?php
+       $routes = [
+       'validateGlobalAddress',
+        'validateUSAddress',
+        'metadata'
+       ];
+       foreach ($routes as $file) {
+           require __DIR__ . '/../src/routes/' . $file . '.php';
+       }
+
